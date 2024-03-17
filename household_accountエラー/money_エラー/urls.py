9a44b171e1777599feb704986fa27fd28e93571e
674nam <moritan972@gmail.com>
2024-03-17 13:58:31 +0900

    リダイレクト先ページ調整

--- household_accountエラー/money_エラー/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'money'
-
-urlpatterns = [
-    path('payment_list/', views.PaymentList.as_view(), name='payment_list'),
-]
--- household_accountエラー/money_エラー/views.py
+++
@@ -1,9 +0,0 @@
-from django.views import generic
-from django.urls import reverse_lazy
-from .models import Payment, PaymentCategory, Income, IncomeCategory
-
-# 支出一覧
-class PaymentList(generic.ListView):
-    template_name = 'money/payment_list.html'
-    model = Payment
-    ordering = '-date'
