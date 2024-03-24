c8acdf947b98f1a93cd32169e6e73ba9f68b89d8
674nam <moritan972@gmail.com>
2024-03-24 23:58:54 +0900

    テスト記述

--- household_account/accounts/tests.py
+++ household_account/accounts/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Family
 
-# Create your tests here.
+class FamilyTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_family_name = "Test Family"
+        cls.family = Family.objects.create(family_name=cls.test_family_name)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_check_family(self):
+        self.assertEqual(str(self.family), f'{str(self.family.id)} , {self.test_family_name}')
+        self.assertEqual(self.family.family_name, self.test_family_name)
+
+
+class UserTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        User = get_user_model()
+        cls.test_user_account_id = "test_account"
+        cls.test_user_email = "test@example.com"
+        cls.test_user_first_name = "Test"
+        cls.test_family = Family.objects.create(family_name="Test Family")
+        cls.test_user = User.objects.create(account_id=cls.test_user_account_id, email=cls.test_user_email, first_name=cls.test_user_first_name, family_name=cls.test_family)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_check_user(self):
+        self.assertEqual(self.test_user.account_id, self.test_user_account_id)
+        self.assertEqual(self.test_user.email, self.test_user_email)
+        self.assertEqual(self.test_user.first_name, self.test_user_first_name)
+        self.assertEqual(str(self.test_user.family_name), f'{str(self.test_family.id)} , {self.test_family.family_name}')
--- household_account/money/tests.py
+++ household_account/money/tests.py
@@ -1,3 +1,79 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import PaymentCategory, IncomeCategory \
+                    , PaymentItem, IncomeItem \
+                    , PaymentOrigItem, IncomeOrigItem \
+                    , Payment, Income
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class MoneyTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        User = get_user_model()
+        cls.test_user = User.objects.create(account_id="test_account",
+                                            email="test@example.com",
+                                            first_name="Test",
+                                            is_superuser=False,
+                                            is_staff=False)
+
+    @classmethod
+    def create_payment_category(self):
+        return PaymentCategory.objects.create(name="Test Payment Category")
+
+    @classmethod
+    def create_income_category(self):
+        return IncomeCategory.objects.create(name="Test Income Category")
+
+    @classmethod
+    def create_payment_item(self, category):
+        return PaymentItem.objects.create(name="Test Payment Item", category=category)
+
+    @classmethod
+    def create_income_item(self, category):
+        return IncomeItem.objects.create(name="Test Income Item", category=category)
+
+    @classmethod
+    def create_payment_orig_item(self, category):
+        return PaymentOrigItem.objects.create(name="Test User Payment Item", category=category, account_id=self.test_user)
+
+    @classmethod
+    def create_income_orig_item(self, category):
+        return IncomeOrigItem.objects.create(name="Test User Income Item", category=category, account_id=self.test_user)
+
+    @classmethod
+    def create_payment(self, category, item):
+        return Payment.objects.create(date=timezone.now(), account_id=self.test_user, category=category, item=item, price=100)
+
+    @classmethod
+    def create_income(self, category, item):
+        return Income.objects.create(date=timezone.now(), account_id=self.test_user, category=category, item=item, price=100)
+
+    def test_check(self):
+        payment_category = self.create_payment_category()
+        self.assertEqual(str(payment_category), "Test Payment Category")
+
+        income_category = self.create_income_category()
+        self.assertEqual(str(income_category), "Test Income Category")
+
+        payment_item = self.create_payment_item(payment_category)
+        self.assertEqual(str(payment_item), f'{payment_category} ; Test Payment Item')
+
+        income_item = self.create_income_item(income_category)
+        self.assertEqual(str(income_item), f'{income_category} ; Test Income Item')
+
+        payment_orig_item = self.create_payment_orig_item(payment_category)
+        self.assertEqual(str(payment_orig_item), f'{payment_category} ; Test User Payment Item')
+
+        income_orig_item = self.create_income_orig_item(income_category)
+        self.assertEqual(str(income_orig_item), f'{income_category} ; Test User Income Item')
+
+        payment = self.create_payment(payment_category, payment_item)
+        self.assertEqual(str(payment), f'{str(payment.date)},{str(payment.price)},{payment.category}')
+
+        income = self.create_income(income_category, income_item)
+        self.assertEqual(str(income), f'{str(income.date)},{str(income.price)},{income.category}')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
